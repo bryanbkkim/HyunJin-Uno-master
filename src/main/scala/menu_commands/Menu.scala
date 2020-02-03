8c2c0fb73767c475e90686fd132c952703ddac9f
Bryan Kim <bryan.kim@westpoint.edu>
2020-02-03 00:02:54 -0500

    v0.1.1

--- src/main/scala/menu_commands/Menu.scala
+++ src/main/scala/menu_commands/Menu.scala
@@ -1,24 +1,39 @@
 package menu_commands
 
-import PlayerOrder._
 
 object Menu {
   
   def initialize() = ???
   
   def showGameArea : String = {
-    var result =  Board.show + "\n"
-    result += Scoreboard.show + "\n"
-    result += CardsRemaining.show
-    
+    var result =                 "Board:\n" +
+                "    [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]    \n" +
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                     [ ] [ ]                 [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "[ ]                                             [ ]\n" + 
+                "    [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ] [ ]    \n" +  
+                "\n" +
+                "Cards in Hand:\n" +
+                "Player1 = 7, Player2 = 7, Player3 = 7, Player4 = 7"
     result
   }
   
   def showPlayerOrder : String = {
-    return PlayerOrder.show
+    var result = "Player1, Player2, Player3, Player4"
+    result
   }
   
-  def advancePlayerOrder : Unit = {
-    PlayerOrder.advance
+  def advancePlayerOrder : String = {
+    var result = "Player2, Player3, Player4, PLayer1"
+    result
   }
 }
--- src/main/scala/menu_commands/Player.scala
+++
@@ -1,6 +0,0 @@
-package menu_commands
-
-//******* PLAYER *******
-class Player (val name : String) {
-  var card = 7
-}
